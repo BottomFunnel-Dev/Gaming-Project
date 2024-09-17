437752c0461102a7df9c8a699d4aa470959d3c8f
Akshay-Kumar2000 <akshaykumar809177@gmail.com>
2024-09-17 18:09:11 +0530

    adminpanel side navbar issue resolved

--- resources/views/admin/user/users.blade.php
+++ resources/views/admin/user/users.blade.php
@@ -1,11 +1,19 @@
 @extends('layouts.main')
 @section('title', 'Users')
 @section('content')
-    <!-- push external head elements to head -->
     @push('head')
-        <link rel="stylesheet" href="{{ asset('plugins/DataTables/datatables.min.css') }}">
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+        <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.4.2/css/responsive.dataTables.min.css">
+        <style>
+            /* Custom styles to make the table scrollable on mobile devices */
+            @media only screen and (max-width: 768px) {
+                .table-responsive {
+                    overflow-x: auto;
+                    -webkit-overflow-scrolling: touch; /* Smooth scrolling on mobile */
+                }
+            }
+        </style>
     @endpush
-
 
     <div class="container-fluid">
         <div class="page-header">
@@ -32,9 +40,7 @@
             </div>
         </div>
         <div class="row">
-            <!-- start message area-->
             @include('include.message')
-            <!-- end message area-->
             <div class="col-md-12">
                 <div class="card p-3">
                     <div class="card-header">
@@ -49,101 +55,80 @@
                         </form>
                     </div>
                     <div class="card-body">
-                        <table id="user_table" class="table">
-                            <thead>
-                                <tr>
-                                    <th>{{ __('#ID') }}</th>
-                                    <th>{{ __('Name') }}</th>
-                                    <th>{{ __('Username') }}</th>
-                                    <th>{{ __('Mobile') }}</th>
-                                    <th>{{ __('Wallet Balance') }}</th>
-                                    <th>{{ __('Referral') }}</th>
-                                    <th>{{ __('Used Referral') }}</th>
-                                    <th>{{ __('Status') }}</th>
-                                    <th>{{ __('Added at') }}</th>
-                                    <th>{{ __('Action') }}</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($users as $key => $val)
+                        <div class="table-responsive"> <!-- Wrapper to make table scrollable -->
+                            <table id="user_table" class="table">
+                                <thead>
                                     <tr>
-                                        <td>{{ $val->id }}</td>
-                                        <td>{{ $val->name }}</td>
-                                        <td>{{ $val->username }}</td>
-                                        <td>{{ $val->mobile }}</td>
-                                        <td>{{ @$val->wallet }}</td>
-                                        <td>{{ @$val->setting->referral }}</td>
-                                        <td>{{ @$val->setting->used_referral }}</td>
-                                        {{-- <td>
-                                            @if (isset($val->setting->status) && $val->setting->status == 1)
-                                                Active
-                                            @elseif(isset($val->setting->status) && $val->setting->status == 0)
-                                                Inactive
-                                            @elseif(isset($val->setting->status) && $val->setting->status == 2)
-                                                Block
-                                            @endif
-                                        </td> --}}
-                                        <td>
-                                            @if (isset($val->setting->status))
-                                                @if ($val->setting->status == 1)
-                                                    Active
-                                                @elseif($val->setting->status == 0)
-                                                    Inactive
-                                                @elseif($val->setting->status == 2)
-                                                    Block
-                                                @endif
-                                            @else
-                                                Inactive
-                                            @endif
-                                        </td>
-
-                                        <td>{{ $val->created_at }}</td>
-                                        <td>
-                                            @can('manage_users')
-                                                <a title="View Statement" href="{{ url('admin/user/statement/' . $val->id) }}">
-                                                    <i class="ik ik-eye f-16 ml-10 text-blue"></i>
-                                                </a>
-                                                <a title="Update user record" href="{{ url('admin/user/edit/' . $val->id) }}">
-                                                    <i class="ik ik-edit-2 f-16 ml-10 text-green"></i>
-                                                </a>
-                                                @php
-                                                    $msg = "'Are you sure want to take this action?'";
-                                                    // Check if the user is new or active
-                                                    if (
-                                                        !isset($val->setting->status) ||
-                                                        (isset($val->setting->status) && $val->setting->status == 1)
-                                                    ) {
-                                                        $sHtml =
-                                                            '<a title="Make User Inactive" href="' .
-                                                            url('admin/user/status/0/' . $val->id) .
-                                                            '">
-                                                                    <i class="ik ik-x f-16 ml-10 text-yellow"></i>
-                                                                  </a>';
-                                                    } else {
-                                                        $sHtml =
-                                                            '<a title="Make User Active" href="' .
-                                                            url('admin/user/status/1/' . $val->id) .
-                                                            '">
-                                                                    <i class="ik ik-check f-16 ml-10 text-blue"></i>
-                                                                  </a>';
-                                                    }
-                                                    echo $sHtml;
-                                                @endphp
-                                                <a title="Update wallet balance"
-                                                    href="{{ url('admin/user/wallet/' . $val->id) }}">
-                                                    <i class="ik ik-dollar-sign f-16 ml-10 text-red"></i>
-                                                </a>
-                                            @endcan
-                                            @can('manage_user')
-                                                <a title="View Statement" href="{{ url('admin/user/statement/' . $val->id) }}">
-                                                    <i class="ik ik-eye f-16 ml-10 text-blue"></i>
-                                                </a>
-                                            @endcan
-                                        </td>
+                                        <th>{{ __('#ID') }}</th>
+                                        <th>{{ __('Name') }}</th>
+                                        <th>{{ __('Username') }}</th>
+                                        <th>{{ __('Mobile') }}</th>
+                                        <th>{{ __('Wallet Balance') }}</th>
+                                        <th>{{ __('Referral') }}</th>
+                                        <th>{{ __('Used Referral') }}</th>
+                                        <th>{{ __('Status') }}</th>
+                                        <th>{{ __('Added at') }}</th>
+                                        <th>{{ __('Action') }}</th>
                                     </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                                </thead>
+                                <tbody>
+                                    @foreach ($users as $key => $val)
+                                        <tr>
+                                            <td>{{ $val->id }}</td>
+                                            <td>{{ $val->name }}</td>
+                                            <td>{{ $val->username }}</td>
+                                            <td>{{ $val->mobile }}</td>
+                                            <td>{{ @$val->wallet }}</td>
+                                            <td>{{ @$val->setting->referral }}</td>
+                                            <td>{{ @$val->setting->used_referral }}</td>
+                                            <td>
+                                                @if (isset($val->setting->status))
+                                                    @if ($val->setting->status == 1)
+                                                        Active
+                                                    @elseif($val->setting->status == 0)
+                                                        Inactive
+                                                    @elseif($val->setting->status == 2)
+                                                        Block
+                                                    @endif
+                                                @else
+                                                    Inactive
+                                                @endif
+                                            </td>
+                                            <td>{{ $val->created_at }}</td>
+                                            <td>
+                                                @can('manage_users')
+                                                    <a title="View Statement" href="{{ url('admin/user/statement/' . $val->id) }}">
+                                                        <i class="ik ik-eye f-16 ml-10 text-blue"></i>
+                                                    </a>
+                                                    <a title="Update user record" href="{{ url('admin/user/edit/' . $val->id) }}">
+                                                        <i class="ik ik-edit-2 f-16 ml-10 text-green"></i>
+                                                    </a>
+                                                    @php
+                                                        $msg = "'Are you sure want to take this action?'";
+                                                        if (
+                                                            !isset($val->setting->status) ||
+                                                            (isset($val->setting->status) && $val->setting->status == 1)
+                                                        ) {
+                                                            $sHtml = '<a title="Make User Inactive" href="' . url('admin/user/status/0/' . $val->id) . '">
+                                                                        <i class="ik ik-x f-16 ml-10 text-yellow"></i>
+                                                                      </a>';
+                                                        } else {
+                                                            $sHtml = '<a title="Make User Active" href="' . url('admin/user/status/1/' . $val->id) . '">
+                                                                        <i class="ik ik-check f-16 ml-10 text-blue"></i>
+                                                                      </a>';
+                                                        }
+                                                        echo $sHtml;
+                                                    @endphp
+                                                    <a title="Update wallet balance" href="{{ url('admin/user/wallet/' . $val->id) }}">
+                                                        <i class="ik ik-dollar-sign f-16 ml-10 text-red"></i>
+                                                    </a>
+                                                @endcan
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                         {{ $users->links() }}
                     </div>
                 </div>
@@ -152,18 +137,12 @@
     </div>
 
     <!-- Load jQuery -->
-    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
-        crossorigin="anonymous"></script>
-
-
+    <script src="https://code.jquery.com/jquery-3.6.0.js" crossorigin="anonymous"></script>
     <!-- Load DataTables -->
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.js"></script>
-
-    <!-- Load DataTables Buttons -->
     <script src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
     <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
 
     <script type="text/javascript" src="https://unpkg.com/xlsx@0.15.1/dist/xlsx.full.min.js"></script>
@@ -173,20 +152,19 @@
             var wb = XLSX.utils.table_to_book(elt, {
                 sheet: "sheet1"
             });
-            return dl ?
-                XLSX.write(wb, {
-                    bookType: type,
-                    bookSST: true,
-                    type: 'base64'
-                }) :
-                XLSX.writeFile(wb, fn || ('users.' + (type || 'xlsx')));
+            return dl ? XLSX.write(wb, {bookType: type, bookSST: true, type: 'base64'}) : XLSX.writeFile(wb, fn || ('users.' + (type || 'xlsx')));
         }
     </script>
-    <!-- push external js -->
+
     @push('script')
-        <script src="{{ asset('plugins/DataTables/datatables.min.js') }}"></script>
-        <script src="{{ asset('plugins/select2/dist/js/select2.min.js') }}"></script>
-        <!--server side users table script-->
-        <script src="{{ asset('js/custom.js') }}"></script>
+        <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/responsive/2.4.2/js/dataTables.responsive.min.js"></script>
+        <script>
+            $(document).ready(function() {
+                $('#user_table').DataTable({
+                    responsive: true
+                });
+            });
+        </script>
     @endpush
 @endsection
