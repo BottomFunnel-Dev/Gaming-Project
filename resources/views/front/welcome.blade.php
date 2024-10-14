1f363fdd7ca947d3d651df802aec4cb85b1e2b4e
Akshay-Kumar2000 <akshaykumar809177@gmail.com>
2024-10-14 13:28:17 +0530

    changes in the main ludo game images

--- resources/views/front/welcome.blade.php
+++ resources/views/front/welcome.blade.php
@@ -59,7 +59,7 @@
                   <div class="gameCard-container">
                     <div class="text-danger"><span class="blink text-danger d-block text-right">◉ Ludo Classic Only (LIVE)</span></div>
                      <a class="gameCard" href="{{url('challenges')}}">
-                        <div class="gameCard-image"><img width="100%" src="{{asset('front/images/ludo-2.jpg')}}" alt=""></div>
+                        <div class="gameCard-image"><img width="100%" src="{{asset('front/images/ludo-2.jpeg')}}" alt=""></div>
                         <div class="gameCard-title">Ludo Classic</div>
                         <div class="gameCard-icon"><img src="{{asset('front/images/award-blue.png')}}" alt=""></div>
                      </a>
@@ -68,7 +68,7 @@
                      <span class="blink text-danger d-block text-right">◉ Ludo Classic Only (LIVE)</span>
                      <a class="gameCard" href="{{url('challenges')}}">
                         <div class="gameCard-image">
-                          <img width="100%" src="{{asset('front/images/ludo-1.jpg')}}" alt=""></div>
+                          <img width="100%" src="{{asset('front/images/ludo-1.jpeg')}}" alt=""></div>
                         <div class="gameCard-title">Ludo Classic</div>
                         <div class="gameCard-icon"><img src="{{asset('front/images/award-blue.png')}}" alt=""></div>
                      </a>
